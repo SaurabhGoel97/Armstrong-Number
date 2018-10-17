62d075091e56038c803f9b4d02ce3e507283d08e
SaurabhGoel97 <31739084+SaurabhGoel97@users.noreply.github.com>
2018-10-17 11:10:24 +0530

    removed comments and gave indentation

--- ArmstrongNumber/ArmstrongNumber/main.cpp
+++ ArmstrongNumber/ArmstrongNumber/main.cpp
@@ -1,9 +1,6 @@
-//Kimberly White
 //Example to check whether an integer is an Armstrong number or not
-
 #include <iostream>
 using namespace std;
-
 int main()
 {
 	int origNum, num, rem, digit, sum = 0;
@@ -13,18 +10,19 @@ int main()
 
 	num = origNum;
 
-	while (num != 0)
+	while (num)
 	{
 		digit = num % 10;
 		sum += digit * digit * digit;
 		num /= 10;
 	}
 	if (sum == origNum)
+	{
 		cout << origNum << " is an Armstrong number.";
+	}
 	else
+	{
 		cout << origNum << " is not an Armstrong number.";
-
-
-	system("pause");
+	}
 	return 0;
 }
